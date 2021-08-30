06552fc0ce61a5613fdfdf8b2019a272a85e44d5
Andrew <andrewmgunn26@gmail.com>
2021-08-30 12:44:04 -0500

    calculated the totals and output with formatted string interpol

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             int cups = 0, cupsPoured = 0, freeCups = 0;
-            double total, tip;
+            double total, tip = 0;
             const double PRICE_OF_LEMONADE = 0.99;
             
             Console.Write("Welcome! How many cups of lemonade would you like to buy?\n>");
@@ -25,6 +25,29 @@ namespace ConsoleApp1
             total = cups * PRICE_OF_LEMONADE;
             
             Console.WriteLine($"Your total is {total:C}");
+            Console.WriteLine("Thank you! Would you like to provide a tip? y/n>");
+            
+            //easy
+           // string choice = Console.ReadLine();
+            
+            //more advanced due to converting input to valid so no error
+            char yN = Console.ReadLine().ToLower()[0];
+
+            if (yN == 'y')
+            {
+                Console.WriteLine("Thank you! Please enter tip amount>");
+                tip = Convert.ToDouble(Console.ReadLine());
+            }
+            
+            Console.WriteLine("\nPrint Transaction details\n---------------");
+            Console.WriteLine($"Total cups sold: {cups}");
+            Console.WriteLine($"Total cups poured: {cupsPoured}");
+            Console.WriteLine($"Total free cups: {freeCups}");
+            Console.WriteLine($"Total cost: {total:C}");
+            Console.WriteLine($"Total tip: {tip:C}");
+            total += tip;
+            Console.WriteLine($"Total money made: {total:C}");
+
         }
     }
 }
