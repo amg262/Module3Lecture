df5b9a548db6bff921b79645805da2d80d1113e3
Andrew <andrewmgunn26@gmail.com>
2021-08-30 12:26:59 -0500

    init commit

--- Program.cs
+++ Program.cs
@@ -6,7 +6,22 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int cups, cupsPoured = 0, freeCups;
+            double total, tip;
+            const double PRICE_OF_LEMONADE = 0.99;
+            
+            Console.Write("Welcome! How many cups of lemonade would you like to buy?\n>");
+            cups = Convert.ToInt32(Console.ReadLine());
+
+            cupsPoured = cups;
+            if (cups >= 3)
+            {
+                freeCups = cups / 3;
+                //Console.WriteLine("Thank you! Because of special, you get {0:N} cups!", freeCups);
+                Console.WriteLine($"Thank you! Because of special, you get {freeCups:N} cups!");
+                cupsPoured += freeCups;
+
+            }
         }
     }
 }
